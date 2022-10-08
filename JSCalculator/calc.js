082627b9615518a4e0b35f783701c8b3a0de78f5
uninumber <110254921+uninumber@users.noreply.github.com>
2022-10-08 12:39:02 -0400

    Add files via upload

---
+++ JSCalculator/calc.js
@@ -0,0 +1,39 @@
+function calculations() {
+	inicio = document.querySelector('input#num1').focus()
+}
+
+function calcular() {
+	let num1 = document.querySelector('input#num1').value
+	let num2 = document.querySelector('input#num2').value
+	let action = document.querySelector('select#operator').value
+	let res = document.querySelector('div#res')
+
+	let total = 0;
+
+
+	if (num1.length < 1 || num2.length < 1) {
+		return alert('Please enter number.')
+	}
+
+	if (action == 'addition') {
+		total = Number(num1) + Number(num2)
+	} else if (action == 'subtraction') {
+		total = num1 - num2
+	} else if (action == 'multiplication') {
+		total = num1 * num2
+	} else {
+		total = num1 / num2
+	}
+
+    res.innerHTML = total.toFixed(2).replace('.', ',')
+}	
+
+function erase() {
+	
+	document.querySelector('input#num1').value = ''
+	
+	document.querySelector('input#num2').value = ''
+
+	document.querySelector('div#res').innerHTML = ''
+	calculations()
+}
